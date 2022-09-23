6e523620fc121511dc78801d1cbc145b66a099c6
Bunch-of-cells <alumin112.welcome@gmail.com>
2022-09-23 14:49:14 +0530

    todo: Better errors

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::fs;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file = fs::read_to_string("ez-modules/test.ez")?;
-    ezlang::parser::parse(&file).unwrap();
+    if let Err(err) = ezlang::parser::parse(&file) {
+        println!("{}", err);
+    }
     Ok(())
 }
